b6750021bb50961ad014b1ace4ebc64623dc1c43
shubham <shubhamsagar628@gmail.com>
2024-04-25 11:03:56 +0530

    Api Testing Done create user and upload image

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,62 +1,89 @@
-import { User } from "../models/user.model.js";
-import { ApiError } from "../utils/ApiError.js";
-import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
-import { uploadOnCloudinary } from "../utils/cloudinary.js";
-
+import {ApiError} from "../utils/ApiError.js"
+import { User} from "../models/user.model.js"
+import {uploadOnCloudinary} from "../utils/cloudinary.js"
+import { ApiResponse } from "../utils/ApiResponse.js";
+import fs from "fs"
 
 const registerUser = asyncHandler( async (req, res) => {
-    
-    //get data from user
-    const {username,email,fullName,password} = req.body 
+    // get user details from frontend
+    // validation - not empty
+    // check if user already exists: username, email
+    // check for images, check for avatar
+    // upload them to cloudinary, avatar
+    // create user object - create entry in db
+    // remove password and refresh token field from response
+    // check for user creation
+    // return res
 
-    //check empty filed
-    if(
-        [username,email,fullName,password].some((filed)=>filed.trim() === "")
-    ){
-        throw new ApiError(404,"All fileds are required")
+
+    const {fullName, email, username, password } = req.body
+   
+
+    if (
+        [fullName, email, username, password].some((field) => field?.trim() === "")
+    ) {
+        throw new ApiError(400, "All fields are required")
     }
 
-    const existingUser = User.findOne({
-        $or:[{username:username},{email:email}]
+    const existedUser = await User.findOne({
+        $or: [{ username }, { email }]
     })
 
-    if(existingUser){
-        throw new ApiError(409,"user Already register with same username and password")
+    if (existedUser) {
+        throw new ApiError(409, "User with email or username already exists")
     }
-    
-    const avatarLocalPath = req.files?.avatar[0]?.path
-    const coverLocalPath = req.files?.coverLocalPath[0]?.path
 
-    if(!avatarLocalPath){
-        throw new ApiError(400,"Avatar file is required")
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    let coverImageLocalPath 
+
+    if(req.files?.coverImage){
+        coverImageLocalPath =req.files?.coverImage[0]?.path
+    }else{
+        coverImageLocalPath=""
+    }
+
+    
+
+    if (!avatarLocalPath) {
+        throw new ApiError(400, "Avatar file is required")
+    }
+    if (!coverImageLocalPath) {
+        coverImageLocalPath =""
+        // throw new ApiError(400, "Avatar file is required")
     }
 
     const avatar = await uploadOnCloudinary(avatarLocalPath)
-    const coverImage = await uploadOnCloudinary(coverLocalPath)
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
 
-    if(!avatar){
-        throw new ApiError(400,"avatar file is required")
+    
+    if (!avatar) {
+        throw new ApiError(400, "Avatar file is required")
     }
 
     const user = await User.create({
         fullName,
-        avatar:avatar.url,
-        coverImage:coverImage.url,
-        email,
-        username:username.toLowerCase()
+        avatar: avatar?.url,
+        coverImage: coverImage?.url  || "",
+        email, 
+        password,
+        username: username.toLowerCase()
     })
 
-    const createdUser  = await User.findById(user._id).select("-password -refreshToken")
+    const createdUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    )
 
-    if(!createdUser){
-        throw new ApiError(500,"Something went wrong while register the user")
+    if (!createdUser) {
+        throw new ApiError(500, "Something went wrong while registering the user")
     }
 
-    return res.status(201).json(
-        new ApiResponse(200,createdUser,"User Created Successfully")
-    )
     
+
+    return res.status(201).json(
+        new ApiResponse(200, createdUser, "User registered Successfully")
+    )
+
 } )
 
 
